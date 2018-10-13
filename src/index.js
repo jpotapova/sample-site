f4827eb023131199c2065d66fae4c6403723b14b
Jelizaveta Potapova <potapova.jelizaveta@gmail.com>
2018-10-14 00:11:56 +0300

    added js to open and close navigation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import './scss/index.scss';
+
+
+(function () {
+
+  const app = document.getElementById("app");
+
+  document.getElementById("nav-open").addEventListener("click", function( event ) {
+    app.className = "app viewnav";
+  }, false);
+
+  document.getElementById("nav-close").addEventListener("click", function( event ) {
+    app.className = "app";
+  }, false);
+
+}());
