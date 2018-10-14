118df4003cc9b195ffce9ea5d1db810113b7134a
Jelizaveta Potapova <potapova.jelizaveta@gmail.com>
2018-10-14 16:25:51 +0300

    grunt setuo server, livereaload, sass

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    sass: {
+      app: {
+        files: {
+           'public/index.css':'src/scss/index.scss'
+         }
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          livereload: true,
+          port: 3000,
+          base: 'public'
+        }
+      }
+    },
+    watch: {
+      options: {
+        livereload: true
+      },
+      scss: {
+        files: '**/*.scss',
+        tasks: ['sass']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+  grunt.registerTask('default', ['sass', 'connect', 'watch']);
+
+};
